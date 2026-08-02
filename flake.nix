a9eda7d0a14177216f9558dee3c0488e9012e7ca
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-08-02 21:53:40 +0900

    feat: simplify nix flake

--- flake.nix
+++ flake.nix
@@ -1,99 +1,68 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    rust-flake.url = "github:juspay/rust-flake";
+    rust-flake.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs =
-    { self, nixpkgs, ... }:
-    let
-      supportedSystems = [
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+      ];
+
+      perSystem =
+        {
+          self',
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            inputsFrom = [ self'.devShells.rust ];
+          };
+
+          packages = {
+            default = self'.packages.ren;
+          };
+
+          rust-project = {
+            src = lib.cleanSourceWith {
+              src = ./.;
+              filter =
+                path: type:
+                config.rust-project.crane-lib.filterCargoSources path type
+                || builtins.any (suffix: lib.hasSuffix suffix path) [
+                  ".md"
+                  ".rhai"
+                  ".yaml"
+                ];
+            };
+            toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              taplo.enable = true;
+              yamlfmt.enable = true;
+            };
+          };
+        };
+
+      systems = [
         "aarch64-darwin"
         "aarch64-linux"
         "x86_64-linux"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = system: nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages = forAllSystems (
-        system:
-        let
-          ren = (pkgsFor system).callPackage ./nix/package.nix { };
-        in
-        {
-          default = ren;
-          inherit ren;
-        }
-      );
-
-      apps = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-          ren = self.packages.${system}.default;
-          app = {
-            type = "app";
-            program = pkgs.lib.getExe ren;
-          };
-        in
-        {
-          default = app;
-          ren = app;
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.mkShellNoCC {
-            packages = with pkgs; [
-              cargo
-              clippy
-              rust-analyzer
-              rustc
-              rustfmt
-              taplo
-            ];
-          };
-        }
-      );
-
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = pkgsFor system;
-        in
-        {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "ren-check";
-            version = "0.0.0";
-            src = pkgs.lib.cleanSource ./.;
-            cargoDepsName = "ren-dependencies";
-            cargoLock.lockFile = ./Cargo.lock;
-            nativeBuildInputs = with pkgs; [
-              clippy
-              rustfmt
-              taplo
-            ];
-            strictDeps = true;
-            buildPhase = ''
-              runHook preBuild
-              cargo fmt --all -- --check
-              cargo clippy --offline --workspace --all-targets -- --deny warnings
-              cargo test --offline --workspace --all-targets
-              taplo lint --colors never
-              taplo format --check --colors never
-              runHook postBuild
-            '';
-            doCheck = false;
-            installPhase = ''
-              touch $out
-            '';
-          };
-        }
-      );
-
-      formatter = forAllSystems (system: (pkgsFor system).nixfmt-tree);
     };
 }
